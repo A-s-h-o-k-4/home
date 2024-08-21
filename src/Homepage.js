63199f224909dacc144ec47df9817cab221468ba
A-s-h-o-k-4 <ashoknaveenkumar0@gmail.com>
2024-08-21 17:01:17 +0530

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,7 +3,7 @@ import './Homepage.css'
 
 function Homepage() {
   return (
-    <div className='main'>
+    <div className='main' id='home'>
       <div className='mainfirst'>
         <h4>Hi everyone, I am</h4>
 
